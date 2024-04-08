5cf9d1286d0dc5bb7b16ec2d373579d117aa6b2b
michaelreinprecht <michael.reinprecht@students.fhv.at>
2024-04-08 17:29:42 +0200

    Refactoring - Created useChatField hook.

--- Frontend/src/Components/Chat/ChatField.tsx
+++ Frontend/src/Components/Chat/ChatField.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { useStompClient } from "react-stomp-hooks";
+import { useChatField } from "./useChatField";
 import ChatFieldButton from "./ChatFieldButton";
 import ChatFieldInput from "./ChatFieldInput";
 
@@ -12,27 +11,7 @@ export default function ChatField({
   activePlayerName,
   lobbyCode,
 }: ChatFieldProps) {
-  const stompClient = useStompClient();
-  const [message, setMessage] = useState("");
-
-  //Uses websockets to send a new message (consisting of playerName and messageText)
-  function sendMessage(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault(); // Prevent default form submission behavior
-    if (message && stompClient) {
-      //Build json message object
-      const chatMessage = {
-        messageSenderName: activePlayerName,
-        messageText: message,
-      };
-      //Send message to websocket
-      stompClient.publish({
-        destination: `/app/${lobbyCode}/chatReceiver`,
-        body: JSON.stringify(chatMessage),
-      });
-      // Clear the input field after sending the message
-      setMessage("");
-    }
-  }
+  const { message, setMessage, sendMessage } = useChatField(activePlayerName, lobbyCode);
 
   return (
     <div className="fixed bottom-4 right-4 w-96">
