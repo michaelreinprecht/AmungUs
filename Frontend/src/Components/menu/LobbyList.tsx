1507e56cd84d84ce777d64c0df9a2446faed76ce
michaelreinprecht <michael.reinprecht@students.fhv.at>
2024-04-09 16:34:34 +0200

    Added key property to LobbyListItem

--- Frontend/src/Components/menu/LobbyList.tsx
+++ Frontend/src/Components/menu/LobbyList.tsx
@@ -36,8 +36,8 @@ export default function LobbyList() {
             <button className="bg-gray-700 text-white px-4 py-2 mb-4 rounded-md shadow-md hover:bg-gray-600" onClick={fetchLobbies}>
                 Refresh
             </button>
-            {lobbies.map((lobby) => (
-                <LobbyListItem lobby={lobby} />
+            {lobbies != undefined && lobbies.map((lobby: Lobby) => (
+                <LobbyListItem key={lobby.lobbyCode} lobby={lobby} />
             ))}
         </div>
     );
