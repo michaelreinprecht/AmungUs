e036a2b57460a02d0f5e023904e93f21d9645de1
Muhammed Güzel <md.guezel@gmail.com>
2024-04-09 18:47:24 +0200

    public game button linked to selectLobby

--- Frontend/src/Components/menu/CreateJoinMenu.tsx
+++ Frontend/src/Components/menu/CreateJoinMenu.tsx
@@ -4,8 +4,12 @@ export default function CreateJoinMenu() {
 
     return (
       <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-        {/* Host Game Button */}
-        <button className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100">
+
+      {/* Host Game Button */}
+      <a href="" className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100">
+        <button
+          className="flex items-center"
+        >
           <img
             src="/character-logo.png"
             className="w-20 h-20 mr-4"
@@ -13,22 +17,28 @@ export default function CreateJoinMenu() {
           />
           <span className="text-xl font-bold">Host Game</span>
         </button>
-        
+      </a>
   
       {/* Public Game Button */}
-      <button
-        className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100"  
-      >
-        <img
-          src="/character-logo.png"
-          className="w-20 h-20 mr-4"
-          alt="Large avatar"
-        />
-        <span className="text-xl font-bold">Public Game</span>
-      </button>
+      <a href="/lobbySelect" className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100">
+        <button
+          className="flex items-center"
+        >
+          <img
+            src="/character-logo.png"
+            className="w-20 h-20 mr-4"
+            alt="Large avatar"
+          />
+          <span className="text-xl font-bold">Public Game</span>
+        </button>
+      </a>
 
-        {/* Private Game Button */}
-        <button className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100">
+
+      {/* Private Game Button */}
+      <a href="" className="flex items-center w-3/4 lg:w-1/2 xl:w-1/3 bg-white border-2 border-gray-300 p-6 rounded-lg shadow-md justify-start hover:bg-gray-100">
+        <button
+          className="flex items-center"
+        >
           <img
             src="/character-logo.png"
             className="w-20 h-20 mr-4"
@@ -36,6 +46,7 @@ export default function CreateJoinMenu() {
           />
           <span className="text-xl font-bold">Private Game</span>
         </button>
+      </a>
       </div>
     );
   }
