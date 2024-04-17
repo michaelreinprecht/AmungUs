43b7d40a5a25f929979603013aa3f2a4042554b8
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-04-17 11:06:57 +0200

    Fixed Link in MainMenu -> now properly sized

--- Frontend/src/Components/menu/MainMenuNavButton.tsx
+++ Frontend/src/Components/menu/MainMenuNavButton.tsx
@@ -12,13 +12,17 @@ export default function MainMenuNavButton({
   link,
 }: MainMenuNavButtonProps) {
   return (
-    <Link href={link} className="w-3/4 mx-auto">
-      <div className="flex items-center mx-auto  xl:w-1/3 bg-gray-800 border-2 border-gray-600 p-6 rounded-lg shadow-md justify-start hover:bg-gray-600 flex-grow">
-        <button className="flex items-center flex-grow">
-          <img src={image} className="w-20 h-20 mr-4" alt="Large avatar" />
-          <span className="text-xl font-bold  text-white">{text}</span>
-        </button>
+    <div className="w-3/4 mx-auto">
+      <div className="w-1/3 mx-auto">
+        <Link href={link}>
+          <div className="flex items-center mx-auto bg-gray-800 border-2 border-gray-600 p-6 rounded-lg shadow-md justify-start hover:bg-gray-600 flex-grow">
+            <button className="flex items-center flex-grow">
+              <img src={image} className="w-20 h-20 mr-4" alt="Large avatar" />
+              <span className="text-xl font-bold  text-white">{text}</span>
+            </button>
+          </div>
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 }
