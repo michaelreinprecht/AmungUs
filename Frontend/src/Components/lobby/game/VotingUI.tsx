ad467bb048cb47e840b6690f69ede583fbb19e68
michaelreinprecht <michael.reinprecht@students.fhv.at>
2024-04-22 09:41:13 +0200

    Added timer to voting UI

--- Frontend/src/Components/lobby/game/VotingUI.tsx
+++ Frontend/src/Components/lobby/game/VotingUI.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from "react";
+
 type VotingUIProps = {
   setIsVotingActive: (isVotingActive: boolean) => void;
   setIsGamePaused: (isGamePaused: boolean) => void;
@@ -7,16 +9,36 @@ export default function VotingUI({
   setIsVotingActive,
   setIsGamePaused,
 }: VotingUIProps) {
+  const [timer, setTimer] = useState(10); // Initial timer value
+
   function stopVoting() {
     setIsVotingActive(false); //Stop the voting
     setIsGamePaused(false); //Resume the game
   }
+
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1); // Decrease timer by 1 every second
+    }, 1000);
+
+    // Clean up the interval when component unmounts or when timer reaches 0
+    return () => clearInterval(timerInterval);
+  }, []);
+
+  // Call stopVoting function after 10 seconds
+  useEffect(() => {
+    if (timer === 0) {
+      stopVoting();
+    }
+  }, [timer]);
 
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-600 w-3/4 h-3/4 flex flex-col justify-center items-center">
       <h1 className="text-white text-xl mb-4">
         Just a placeholder for the real voting UI
       </h1>
+      <p className="text-white text-lg mb-2">Remaining voting time: {timer}</p>{" "}
+      {/* Display timer here */}
       <button
         onClick={stopVoting}
         className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full shadow-md focus:outline-none focus:shadow-outline"
