aa0a9d92c6c978dad4af23b2bf0e47b76c704730
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-05-08 08:45:33 +0200

    Removed old backend

--- AmungUs/src/main/java/com/example/messagingstompwebsocket/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.messagingstompwebsocket;
-
-//Class for useful methods that will likely be used more than once
-public class Utils {
-    public static float getDistanceBetween(float xPos1, float yPos1, float xPos2, float yPos2) {
-        return (float) Math.sqrt(Math.pow(xPos2 - xPos1, 2) + Math.pow(yPos2 - yPos1, 2));
-    }
-}
