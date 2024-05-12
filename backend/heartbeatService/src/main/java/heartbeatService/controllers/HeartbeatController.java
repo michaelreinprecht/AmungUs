06c7ab82d8a97e0b3ee5a249917eaedd12c9a365
michaelreinprecht <michael.reinprecht@students.fhv.at>
2024-05-12 20:12:51 +0200

    Tried synchronizing heartbeat ...

--- backend/heartbeatService/src/main/java/heartbeatService/controllers/HeartbeatController.java
+++ backend/heartbeatService/src/main/java/heartbeatService/controllers/HeartbeatController.java
@@ -19,7 +19,7 @@ public class HeartbeatController {
 
 	@MessageMapping("/{lobbyCode}/heartbeatReceiver")
 	@SendTo("/lobby/{lobbyCode}/heartbeat")
-	public boolean heartbeats(@DestinationVariable String lobbyCode, String playerName) throws Exception {
+	public synchronized boolean heartbeats(@DestinationVariable String lobbyCode, String playerName) throws Exception {
 		logger.debug("Received heartbeat request for lobby code: {}", lobbyCode);
 		// Get the lobby from the lobby service
 
@@ -28,8 +28,8 @@ public class HeartbeatController {
 			heartbeatLobbyService.createLobby(new HeartbeatLobby(lobbyCode), playerName);
 		} else {
 			logger.info("Heartbeat received for player: {}", playerName + " in lobby: " + lobbyCode);
-			heartbeatLobbyService.updateLobby(lobbyCode, playerName);
-		}
+		}			heartbeatLobbyService.updateLobby(lobbyCode, playerName);
+
 
 		return false;
 	}
