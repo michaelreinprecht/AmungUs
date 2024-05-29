306958f0628747f5a3ce0d7df8345dfc45e0515b
michaelreinprecht <reinprecht.michael.business@gmail.com>
2024-05-29 11:18:38 +0200

    Changed to server address

--- Frontend/src/app/globals.tsx
+++ Frontend/src/app/globals.tsx
@@ -55,5 +55,5 @@ export const characterOptions = [
   },
 ];
 
-//export const serverAddress = "10.0.40.167";
-export const serverAddress = "localhost";
+export const serverAddress = "10.0.40.167";
+//export const serverAddress = "localhost";
